813e02882c2672682151a9d673a5f4e01ff5d4a8
12Froggo <shalithamza@gmail.com>
2022-10-11 14:39:19 +0300

    push

--- Renk Algılama/main.py
+++ Renk Algılama/main.py
@@ -1,21 +1,19 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    lower_green = np.array([110, 50, 50])
-    upper_green = np.array([130, 255, 255])
-    green_mask = cv2.inRange(hsv_frame, lower_green, upper_green)
-    
-    cv2.imshow("Webcam", frame)
-    cv2.imshow("Green Mask", green_mask)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+while(1):
+    _, frame = cap.read()
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    res = cv.bitwise_and(frame,frame, mask= mask)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask)
+    cv.imshow('res',res)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
         break
-
-cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
