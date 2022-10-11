824badc25f1a06e445a9d4f4a0f0399e0da58baf
12Froggo <shalithamza@gmail.com>
2022-10-11 13:49:45 +0300

    push

--- Renk Algılama/main.py
+++ Renk Algılama/main.py
@@ -8,8 +8,8 @@ while True:
     frame = cv2.flip(frame, 1)
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower_green = np.array([0, 0, 0])
-    upper_green = np.array([180, 255, 255])
+    lower_green = np.array([110, 50, 50])
+    upper_green = np.array([130, 255, 255])
     green_mask = cv2.inRange(hsv_frame, lower_green, upper_green)
     
     cv2.imshow("Webcam", frame)
