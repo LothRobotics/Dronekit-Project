c9379c3a354179eec91809b2669dcee03e14c0e3
12Froggo <shalithamza@gmail.com>
2022-10-12 09:53:15 +0300

    push

--- Renk Algılama/main.py
+++ Renk Algılama/main.py
@@ -1,19 +1,20 @@
-import cv2 as cv
+import cv2
 import numpy as np
-
-cap = cv.VideoCapture(0)
-
-while(1):
-    _, frame = cap.read()
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
-    cv.imshow('res',res)
-    k = cv.waitKey(5) & 0xFF
-    if k == 27:
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # blue
+    lower_blue = np.array([100, 150, 0])
+    upper_blue = np.array([140, 255, 255])
+    blue_mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
+    blue = cv2.bitwise_and(frame, frame, mask = blue_mask)
+    cv2.imshow("Webcam", frame)
+    # cv2.imshow("Green Mask", green_mask)
+    cv2.imshow("Blue", blue)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-cv.destroyAllWindows()
+
+cap.release()
+cv2.destroyAllWindows()
