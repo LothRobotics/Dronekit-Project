3d90c2437d4e5ac9c9b7877fc0141f3fc0419b4d
Wragnardev <shalithamza@gmail.com>
2022-11-15 21:44:12 +0300

    push

---
+++ main.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+
+import time, math
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+    This method is an approximation, and will not be accurate over large distances and close to the 
+    earth's poles. It comes from the ArduPilot test code: 
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+vehicle = connect("udp:127.0.0.1:1451", wait_ready=True)
+
+def arm_and_takeoff(aTargetAltitude):
+    
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+arm_and_takeoff(20)
+
+vehicle.airspeed = 3
+
+point1 = LocationGlobalRelative(-35.3626485, 149.1654485, 20)
+
+print("ilk wp gidiliyor")
+
+vehicle.simple_goto(point1)
+
+while get_distance_metres(vehicle.location.global_frame, point1)>2:
+    vehicle.simple_goto(point1)
+    time.sleep(0.5)
+print("point1'e ulaşıldı.")    
+time.sleep(20)
+
+vehicle.mode = VehicleMode("RTL")
+vehicle.close()
